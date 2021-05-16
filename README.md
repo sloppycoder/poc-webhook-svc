94c128bebb1d791d84f20927a9dfff787bbc752b
Li Lin <guru.lin@gmail.com>
2021-05-16 20:54:37 +0800

    added db connection. enabled actuator endpoints.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # POC invoking webhook 
 
 More to come.
+
+
+## Development
+### create test database
+```sql
+create database poc;
+create user pocuser with encrypted password 'password';
+grant all privileges on database poc to pocuser;
+
+```
--- src/main/java/org/vino9/ms/webhooksvc/WebhookSvcApplication.java
+++ src/main/java/org/vino9/ms/webhooksvc/WebhookSvcApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebhookSvcApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebhookSvcApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(WebhookSvcApplication.class, args);
+  }
 }
