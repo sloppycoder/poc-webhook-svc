6f97315a3eaecdaf6b96430d882dc612181b2a1e
Li Lin <guru.lin@gmail.com>
2021-05-16 23:35:57 +0800

    added wiremock dependency

--- src/test/java/org/vino9/ms/webhooksvc/WebhookSvcApplicationTests.java
+++ src/test/java/org/vino9/ms/webhooksvc/WebhookSvcApplicationTests.java
@@ -1,13 +1,23 @@
 package org.vino9.ms.webhooksvc;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import de.mkammerer.wiremock.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
 
 @SpringBootTest
 class WebhookSvcApplicationTests {
+  @RegisterExtension WireMockExtension wireMock = new WireMockExtension();
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {}
 
+  @Test
+  void test() {
+    wireMock.stubFor(WireMock.get("/hello").willReturn(WireMock.ok("world")));
+    URI uri = wireMock.getBaseUri().resolve("/hello");
+  }
 }
