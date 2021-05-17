e7208b4bfce3f4a6c3764d73c6c872110c7e4c5c
Li Lin <guru.lin@gmail.com>
2021-05-18 00:38:45 +0800

    fix SQL logic error.

--- src/main/java/org/vino9/ms/webhooksvc/data/WebhookRequestRepository.java
+++ src/main/java/org/vino9/ms/webhooksvc/data/WebhookRequestRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 public interface WebhookRequestRepository extends ReactiveCrudRepository<WebhookRequest, Long> {
-    @Query("select * from webhook_requests where status = 'NEW' or status = 'RETRY' and retry_after > current_timestamp")
+    @Query("select * from webhook_requests where status = 'NEW' or status = 'RETRY' and retry_after < current_timestamp")
     Flux<WebhookRequest> findPendingRequests();
 }
